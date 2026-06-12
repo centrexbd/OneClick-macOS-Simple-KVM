881ad104bf89ef576d2fc46ea2a2cc4beeb7d2cf
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2026-06-12 09:10:37 -0400

    Update fetch-macOS-v2.py

--- fetch-macOS-v2.py
+++ fetch-macOS-v2.py
@@ -467,8 +467,8 @@ def main():
             {"name": "Monterey (12)", "b": "Mac-B809C3757DA9BB8D", "m": "00000000000000000", "os_type": "latest", "short": "monterey"},
             {"name": "Ventura (13)", "b": "Mac-B4831CEBD52A0C4C", "m": "00000000000000000", "os_type": "latest", "short": "ventura"},
             {"name": "Sonoma (14)", "b": "Mac-827FAC58A8FDFA22", "m": "00000000000000000", "short": "sonoma"},
-            {"name": "Sequoia (15)", "b": "Mac-7BA5B2D9E42DDD94", "m": "00000000000000000", "short": "Sequoia", "os_type": "latest"},
-            {"name": "Tahoe (16) - NOT Recommended", "b": "Mac-27AD2F918AE68F61", "m": "00000000000000000", "short": "Tahoe", "os_type": "latest"}
+            {"name": "Sequoia (15)", "b": "Mac-7BA5B2D9E42DDD94", "m": "00000000000000000", "short": "sequoia", "os_type": "latest"},
+            {"name": "Tahoe (16) - NOT Recommended", "b": "Mac-27AD2F918AE68F61", "m": "00000000000000000", "short": "tahoe", "os_type": "latest"}
     ]
 
     for index, product in enumerate(products):
