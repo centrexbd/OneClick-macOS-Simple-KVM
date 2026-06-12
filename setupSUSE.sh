c02ec0d0033aa300d2a4bd9e1fd1d08bc1bbc2a6
notAperson535 <95918679+notAperson535@users.noreply.github.com>
2026-06-12 09:08:32 -0400

    change order of deletion

--- setupSUSE.sh
+++ setupSUSE.sh
@@ -5,12 +5,12 @@ sudo zypper in qemu-tools qemu-kvm qemu-x86 qemu-audio-pa python3-pip --no-confi
 
 (ls macOS.qcow2 >> /dev/null 2>&1 && echo "") || qemu-img create -f qcow2 macOS.qcow2 100G
 
+(ls RecoveryImage.dmg >> /dev/null 2>&1 && rm RecoveryImage.dmg) || echo ""
+
+(ls BaseSystem.dmg >> /dev/null 2>&1 && rm BaseSystem.dmg) || echo ""
+
 ./fetch-macOS-v2.py
 
 (ls RecoveryImage.dmg >> /dev/null 2>&1 && mv RecoveryImage.dmg BaseSystem.dmg) || echo ""
 
 qemu-img convert BaseSystem.dmg -O raw BaseSystem.img
-
-(ls RecoveryImage.dmg >> /dev/null 2>&1 && rm RecoveryImage.dmg) || echo ""
-
-(ls BaseSystem.dmg >> /dev/null 2>&1 && rm BaseSystem.dmg) || echo ""
